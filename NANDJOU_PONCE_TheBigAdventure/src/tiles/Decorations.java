82786e1f80cd9a5cf0ab98e6b56af3bb3868d679
F1rePh0enix <128370589+F1rePh0enix@users.noreply.github.com>
2023-12-03 19:20:32 +0100

    new package -tiles
    Added the previous lone Java files to the folder

---
+++ NANDJOU_PONCE_TheBigAdventure/src/tiles/Decorations.java
@@ -0,0 +1,5 @@
+package tiles;
+
+public enum Decorations {
+    ALGAE, CLOUD, FLOWER, FOLIAGE, GRASS, LADDER, LILY, PLANK, REED, ROAD, SPROUT, TILE, TRACK, VINE
+}
---
+++ NANDJOU_PONCE_TheBigAdventure/src/tiles/Nutritious.java
@@ -0,0 +1,5 @@
+package tiles;
+
+public enum Nutritious {
+    BANANA, BOBA, BOTTLE, BURGER, CAKE, CHEESE, DONUT, DRINK, EGG, FRUIT, FUNGUS, FUNGI, LOVE, PIZZA, POTATO, PUMPKIN, TURNIP
+}
