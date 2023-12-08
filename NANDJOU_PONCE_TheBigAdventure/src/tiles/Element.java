03e6c7b6d59855faffd3700353bd6d17034b01a2
F1rePh0enix <jules.ponce@eleves.enpc.fr>
2023-12-08 13:24:12 +0100

    Update Element.java

--- NANDJOU_PONCE_TheBigAdventure/src/tiles/Element.java
+++ NANDJOU_PONCE_TheBigAdventure/src/tiles/Element.java
@@ -1,5 +1,29 @@
 package tiles;
 
-public class Element {
+import java.util.List;
+import java.util.Optional;
+import map.PointInt;
 
+public class Element {
+	Optional <String> name;
+	public String skin;
+	public PointInt position;
+	Optional <Integer> health;
+	String kind;
+	Optional<PointInt> zoneDep;
+	Optional<PointInt> zoneVar;
+	Optional<String> behavior;
+	Optional<Integer> damage;
+	Optional<String> text;
+	Optional<List<String>> steal;
+	Optional<List<String>> trade;
+	Optional<Boolean> locked;
+	Optional<PointInt> flow;
+	Optional<Boolean> phantomized;
+	Optional<PointInt> teleport;
+	
+	Element(String skin, PointInt position, String kind){
+		
+	}
+	
 }
