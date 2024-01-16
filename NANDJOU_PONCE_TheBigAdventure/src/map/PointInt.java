007c779e0c45f51f4dcddcd180883450c53cc71d
F1rePh0enix <jules.ponce@eleves.enpc.fr>
2024-01-16 13:51:29 +0100

    element preliminaire

--- NANDJOU_PONCE_TheBigAdventure/src/map/PointInt.java
+++ NANDJOU_PONCE_TheBigAdventure/src/map/PointInt.java
@@ -2,4 +2,6 @@ package map;
 
 public class PointInt {
 	public int x; public int y;
+	public PointInt(int x, int y){
+	}
 }
--- NANDJOU_PONCE_TheBigAdventure/src/tiles/Element.java
+++ NANDJOU_PONCE_TheBigAdventure/src/tiles/Element.java
@@ -1,11 +1,13 @@
 package tiles;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import map.PointInt;
 import parser.Lexer;
 import parser.Result;
+import parser.Token;
 
 public class Element {
 	public Optional <String> name;
@@ -35,16 +37,81 @@ public class Element {
 			
 			case IDENTIFIER :
 				switch(res.content()) {
-				case "name" : break;
-				case "skin" : break;
-				case "position" : break;
-				case "health" : break;
-				case "zone" : break;
-				case "behavior" : break;
-				case "damage" : break;
-				case "text" : break;
-				case "steal" : break;
+				case "name" : 
+					res=lexer.nextResult();// Itll be ":"
+					res=lexer.nextResult();
+					name.of(res.content());
+					break;
+				case "skin" : 
+					res=lexer.nextResult();
+					res=lexer.nextResult();
+					skin.of(res.content());
+					break;
+				case "position" : 
+					var pos=new PointInt(0,0);
+					res=lexer.nextResult();
+					res=lexer.nextResult();
+					res=lexer.nextResult();
+					pos.x=Integer.parseInt(res.content());
+					res=lexer.nextResult();
+					res=lexer.nextResult();
+					pos.y=Integer.parseInt(res.content());
+					position.of(pos);
+					break;
+				case "health" : 
+					res=lexer.nextResult();
+					res=lexer.nextResult();
+					int hp=Integer.parseInt(res.content());
+					health.of(hp);
+					break;
+				case "zone" : 
+					res=lexer.nextResult();
+					res=lexer.nextResult();
+					res=lexer.nextResult();
+					var zoneD=new PointInt(0,0);
+					zoneD.x=Integer.parseInt(res.content());
+					res=lexer.nextResult();
+					res=lexer.nextResult();
+					zoneD.y=Integer.parseInt(res.content());
+					var zoneV=new PointInt(0,0);
+					res=lexer.nextResult();
+					res=lexer.nextResult();
+					res=lexer.nextResult();
+					zoneV.x=Integer.parseInt(res.content());
+					res=lexer.nextResult();
+					res=lexer.nextResult();
+					zoneV.y=Integer.parseInt(res.content());
+					zoneDep.of(zoneD);
+					zoneVar.of(zoneV);
+					break;
+				case "behavior" : 
+					res=lexer.nextResult();
+					res=lexer.nextResult();
+					behavior.of(res.content());
+					break;
+				case "damage" : 
+					res=lexer.nextResult();
+					res=lexer.nextResult();
+					damage.of(Integer.parseInt(res.content()));
+					break;
+				case "text" : 
+					res=lexer.nextResult();
+					res=lexer.nextResult();
+					text.of(res.content());//Will need the """ again
+					break;
+				case "steal" : 
+					res=lexer.nextResult();
+					res=lexer.nextResult();
+					var stl = new ArrayList<String>();
+					while (res.token() != Token.RIGHT_BRACKET) {
+						stl.add(res.content());
+						res=lexer.nextResult();
+						res=lexer.nextResult();
+					}
+					steal.of(stl);
+					break;
 				case "trade" : break;
+				case "locked": break;
 				case "flow" : break;
 				case "phantomized" : break;
 				case "teleport" : break;
