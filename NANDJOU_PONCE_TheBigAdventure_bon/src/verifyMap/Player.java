d889c4ed34fa9e714832d4cf2dbbedb676c411bd
F1rePh0enix <jules.ponce@eleves.enpc.fr>
2024-01-16 17:13:36 +0100

    Readded element.java

--- NANDJOU_PONCE_TheBigAdventure_bon/src/verifyMap/Player.java
+++ NANDJOU_PONCE_TheBigAdventure_bon/src/verifyMap/Player.java
@@ -3,13 +3,13 @@ package verifyMap;
 public class Player {
 	private String name;
 	private boolean player;
-	private Position position;
+	//private Position position;
 	private String skin;
 	private int health;
 	private String kind;
 	private int damage;
 	private String behavior;
-	private Zone zone;
+	//private Zone zone;
 	
 	
 }
